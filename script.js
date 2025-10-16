d3f94cfb8d94262d66e31c4ea4234dcc36a09a53
Arashad Ahamad <96arashad@gmail.com>
2025-10-16 18:33:04 +0530

    Fix hamburger menu and update content

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const hamburger = document.querySelector('.hamburger')
 const navLink = document.querySelector('.nav-links')
 const hamburgerFirstChild = document.querySelector('.fa-solid')
-hamburger.addEventListener('click', () => {
+const allNavLinks = document.querySelectorAll('.nav-links a')
+
+hamburger.addEventListener('click', (e) => {
+  e.stopPropagation()
   navLink.classList.toggle('hamActive')
 
   if (navLink.classList.contains('hamActive')) {
@@ -9,4 +12,18 @@ hamburger.addEventListener('click', () => {
   } else {
     hamburgerFirstChild.classList.replace('fa-xmark', 'fa-bars')
   }
+})
+
+document.addEventListener('click', (e) => {
+  if (!hamburger.contains(e.target) && !navLink.contains(e.target)) {
+    navLink.classList.remove('hamActive')
+    hamburgerFirstChild.classList.replace('fa-xmark', 'fa-bars')
+  }
+})
+
+allNavLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navLink.classList.remove('hamActive')
+    hamburgerFirstChild.classList.replace('fa-xmark', 'fa-bars')
+  })
 })
